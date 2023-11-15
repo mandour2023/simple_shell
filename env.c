151afbf3514462e63b6ad22bb73d2826f43c6bb8
ahmed <ahmed2004ali2010@gmail.com>
2023-11-15 05:09:19 -0800

    env.c

--- env.c
+++ env.c
@@ -1,93 +1,92 @@
-#include "custom_shell.h"
+#include "shell.h"
 
 /**
- * display_custom_environment - prints the current custom environment
- * @data: Structure containing potential arguments. Used to maintain
+ * _myctx - prints the current environment
+ * @ctx: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * Return: Always 0
  */
-int display_custom_environment(custom_data_t *data)
+int _myctx(ctx_t *ctx)
 {
-    print_string_list(data->environment);
-    return (0);
+	print_list_str(ctx->env);
+	return (0);
 }
 
 /**
- * get_custom_environment_value - gets the value of a custom environment variable
- * @data: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- * @name: custom environment variable name
+ * _myvalue - gets the value of an environ variable
+ * @ctx: Structure containing potential arguments. Used to maintain
+ * @key: env var name
  *
  * Return: the value
  */
-char *get_custom_environment_value(custom_data_t *data, const char *name)
+char *_myvalue(ctx_t *ctx, const char *key)
 {
-    list_t *node = data->environment;
-    char *value;
+	list_t *node = ctx->env;
+	char *p;
 
-    while (node)
-    {
-        value = starts_with(node->str, name);
-        if (value && *value)
-            return value;
-        node = node->next;
-    }
-    return NULL;
+	while (node)
+	{
+		p = starts_with(node->str, key);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
 }
 
 /**
- * set_custom_environment - Initialize a new custom environment variable,
- *                         or modify an existing one
- * @data: Structure containing potential arguments. Used to maintain
+ * _mysetup - Initialize a new environment variable,
+ *             or modify an existing one
+ * @ctx: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * Return: Always 0
  */
-int set_custom_environment(custom_data_t *data)
+int _mysetup(ctx_t *ctx)
 {
-    if (data->arg_count != 3)
-    {
-        print_error("Incorrect number of arguments\n");
-        return 1;
-    }
-    if (set_environment_variable(data, data->arg_list[1], data->arg_list[2]))
-        return 0;
-    return 1;
+	if (ctx->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(ctx, ctx->argv[1], ctx->argv[2]))
+		return (0);
+	return (1);
 }
 
 /**
- * unset_custom_environment - Remove a custom environment variable
- * @data: Structure containing potential arguments. Used to maintain
+ * _myunset - Remove an environment variable
+ * @ctx: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * Return: Always 0
  */
-int unset_custom_environment(custom_data_t *data)
+int _myunset(ctx_t *ctx)
 {
-    int i;
+	int i;
 
-    if (data->arg_count == 1)
-    {
-        print_error("Too few arguments.\n");
-        return 1;
-    }
-    for (i = 1; i <= data->arg_count; i++)
-        unset_environment_variable(data, data->arg_list[i]);
+	if (ctx->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= ctx->argc; i++)
+		_unsetenv(ctx, ctx->argv[i]);
 
-    return 0;
+	return (0);
 }
 
 /**
- * populate_custom_environment_list - populates custom environment linked list
- * @data: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _populate - populates env linked list
+ * @ctx: Structure containing potential arguments. Used to maintain
+ *            constant function prototype.
  * Return: Always 0
  */
-int populate_custom_environment_list(custom_data_t *data)
+int _populate(ctx_t *ctx)
 {
-    list_t *node = NULL;
-    size_t i;
+	list_t *node = NULL;
+	size_t i;
 
-    for (i = 0; data->environment_variables[i]; i++)
-        add_node_end(&node, data->environment_variables[i], 0);
-    data->environment = node;
-    return 0;
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	ctx->env = node;
+	return (0);
 }
